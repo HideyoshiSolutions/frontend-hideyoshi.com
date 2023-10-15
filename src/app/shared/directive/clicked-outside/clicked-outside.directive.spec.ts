81248619446438ea76118a2349988b8cafcf4004
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2023-10-14 23:52:49 -0300

    Removes Unused Angular Files

--- src/app/shared/directive/clicked-outside/clicked-outside.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClickedOutsideDirective } from './clicked-outside.directive';
-
-describe('ClickedOutsideDirective', () => {
-    it('should create an instance', () => {
-        const directive = new ClickedOutsideDirective();
-        expect(directive).toBeTruthy();
-    });
-});
