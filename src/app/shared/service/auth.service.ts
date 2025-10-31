401879275cfc82f21b213269bb77fe38784686e2
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2025-10-31 18:29:07 -0300

    chore: updates angular to v18

--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {first, map, Observable, of, Subject,} from 'rxjs';
 import {catchError} from 'rxjs/operators';
